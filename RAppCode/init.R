688678240411d30eb3d5729d20424940c16aa78d
antoine.lizee <antoine.lizee@ucsf.edu>
2014-03-27 12:17:11 -0700

    code for the app

--- RAppCode/init.R
+++ RAppCode/init.R
@@ -1,32 +1,58 @@
 
+simulateDist <- function(n, mu, sd, distFamily="gaussian") {
+  switch(distFamily,
+         "gaussian" = rnorm(n,mu,sd),
+         "poisson" = rpois(n,mu),
+         stop("ARGS_ERR: distribution family not recognised or suported"))
+}
 
-
-getPValue <- function(mux, mu0 = 0, dmu = NA, sd,  nx = 10,  simulate = F, nSimulations = 10000, family = "gaussian") {
+getPValue <- function(mux, mu0 = 1, sd,  nx = 9,  simulate = F, nSimulations = 10000, distFamily = "gaussian") {
   # Specify the default value for sd 
-  if missing(sd) {
-    sd = switch(family,
+  if (missing(sd)) {
+    sd = switch(distFamily,
                 "gaussian" = 1,
                 "poisson" = NA,
                 NA)
   }
   # Get the analytical results
   if (!simulate) {
-    if (family == "gaussian") {
+    if (distFamily == "gaussian") {
       df <- nx - 1
       stderr <- sd/sqrt(nx)
-      if (is.na(dmu)) {
-        dmu = mux - mu0
-      }
-      tstat <- dmu/stderr
+      tstat <- (mux - mu0)/stderr
       return(pt(-abs(tstat), df))
-    } elseif (family == "poisson") {
-      if (!is.na(sd)) Warning("sd is not used")
+    } else if (distFamily == "poisson") {
+      if (!is.na(sd)) warning("ARG_WARN: sd is not used for univariate distribution family ", distFamily)
+      if (mu0 < mux) {
+        tstat <- ppois(nx * mu0, nx * mux, lower.tail=T)
+      } else {
+        tstat <- ppois(nx * mu0, nx * mux, lower.tail=F)
+      }
     }
   } 
   # Get the simulated results
   else {
-    muMat <- replicate(n=10000, expr= mean()
+    muVec <- replicate(n=nSimulations, expr = sum(simulateDist(nx,mux,sd,distFamily))) / nx
+    if (mu0 < mux) {
+      return(mean(muVec<mu0))
+    } else {
+      return(mean(muVec>mu0))
+    }
   }
 }
 
 
+
+mux <- 2
+nx <- 3
+nSimulations <- 10000
+# muVec <- replicate(n=nSimulations, expr = sum(simulateDist(nx,mux,sd,distFamily))) / nx
+xVec <- sapply(1:nSimulations, function(ni) {simulateDist(nx,mux,sd,distFamily)})
+muVec <- sum()
+X <- seq(from=min(muVec), to=max(muVec), length.out=nSimulations/50)
+hist(muVec,X, freq=F )
+lines(density(muVec, bw="SJ"), col = "green", lwd = 3)
+lines(X, dnorm(X, mux, sd) , col = "blue", lwd = 2)
+lines(X, dt((X-mux)*sqrt(nx)/sd, df=(nx-1)) * sqrt(nx)*sd, col = "red", lwd = 2)
+# lines(X, dnorm(X, mux, sd) * 50 * diff(range(muVec)), col = "blue", lwd = 2)
+# lines(X, dt((X-mux)*sqrt(nx)/sd, df=(nx-1)) * sqrt(nx)*sd* 50 * diff(range(muVec)), col = "red", lwd = 2)
