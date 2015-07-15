40bba8ad7d2ef3ebda852f0134f7b6d581b23353
antoine.lizee <antoine.lizee@ucsf.edu>
2015-07-15 16:12:40 -0700

    working check() tests for noneType and integer arguments

--- rapid/objects.R
+++ rapid/objects.R
@@ -6,6 +6,8 @@ library(proto)
 newWithDefaults <- function(protoName, protoArgs) {
   # This function creates a constructor for a proto trait, but checking for missing values when these are required.
   # The required values are specified by setting their default Value as "NULL".
+  # The only significant disadvantage of using this function is the lack of auto-completion when creating new objects,
+  # because of the use of the ellipsis in the returned function. Could be fixed by using the formals()<- setter. TODO
   return(function(., ...) {
     # Collect the arguments passed to .$new()
     newArgs <- list(...)
@@ -34,29 +36,97 @@ newWithDefaults <- function(protoName, protoArgs) {
 
 # Argument helpers --------------------------------------------------------
 
+checkWithinValues <- function(., value) {
+  values <- .$expectedValues
+  if (isAnNA(values) || isAnNA(value) || value %in% values) {
+    return(value)
+  }
+  stop(simpleError(
+    buildArgErrorMessage(., "Wrong value for ")
+  ))
+}
+
+checkWithinRange <- function(., value) {
+  rethrowWithMessage({
+    min <- .$expectedValues$min
+    max <- .$expectedValues$max
+    isInRange <- value >= .$expectedValues$min && value <= .$expectedValues$max
+  }, buildApiDevErrorMessage(., "Wrong range specification."))
+  if (isInRange %in% FALSE) { #NA passes
+    stop(simpleError(buildArgErrorMessage(., "Value is not in range for ")))
+  }
+  return(value)
+}
+
 defaultArgumentCheck <- function(., value) {
   # This is the default argument check function. If there is expected values specified,
   # it checks that the passed argument is wihin these.
   # It also does some basic type checks.
   # TODO: support lambda & other functions.
   switch(.$expectedValueType,
-         "string" = checkWithinValues(., value, .$expectedValues),
-         "int"=, "integer"= checkWithinRange(., value))
-  
+         "string" = checkWithinValues(., value),
+         "integer"= {
+           value = throwWithMessage(toInt(value), buildArgErrorMessage(., "Wrong value type for "))
+           if (is.list(.$expectedValues)) {
+             checkWithinRange(., value)
+           } else {
+             checkWithinValues(., value)
+           }
+         },
+         "any" = checkWithinValues(., value))
 }
 
-checkWithinExpectedValues <- function(., value, values) {
-  if (!is.na(values) & !is.null(.$expectedValues, values)) {
-    if (!value %in% values) {
-      stop(simpleError(
-        paste0("ARGS: Wrong Value for "
-               , .$name
-               , ifelse(is.na(.$description), "", paste0(" the ", .$description))
-               , ". Please refer to the embedded reference available with an OPTIONS call to "
-               , .$rapid.action
-               , "."
-        )))
-    }
+
+# Misc Helpers ------------------------------------------------------------
+
+isAnNA <- function(x) {
+  length(x) == 1 && is.na(x)
+}
+
+buildApiDevErrorMessage <- function(., msg) {
+  paste0("API_DEV::ARG: "
+         , msg
+         , .$name
+         , ifelse(is.na(.$description), "", paste0(" the ", .$description))
+         , ". Please check your argument definitions for the action '"
+         , .$rapid.action
+         , "'."
+  )
+}
+
+buildArgErrorMessage <- function(., msg) {
+  paste0("ARGS: "
+         , msg
+         , .$name
+         , ifelse(is.na(.$description), "", paste0(" the ", .$description))
+         , ". Please refer to the embedded reference available with an OPTIONS call to '"
+         , .$rapid.action
+         , "'."
+  )
+}
+
+throwWithMessage <- function(expr, errorMsg) {
+  # This is for a custom error
+  tryCatch(expr = expr, error = function(e) stop(simpleError(errorMsg)))
+}
+
+rethrowWithMessage <- function(expr, errorMsg) {
+  # This is for a native error for which we customize the output
+  tryCatch(expr = expr, error = function(e) {
+    e$message <- paste0(errorMsg, "\nOriginal error msg:\n", e$message)
+    stop(e)
+  })
+}
+
+toInt <- function(string) {
+  if (string == "") {
+    return(NA)
+  } 
+  res <- strtoi(string)
+  if (is.na(res)) {
+    stop()
+  } else {
+    return(res)
   }
 }
 
@@ -64,13 +134,13 @@ checkWithinExpectedValues <- function(., value, values) {
 # Argument definition -----------------------------------------------------
 
 Argument <- proto(new = newWithDefaults(protoName = "Argument",
-                                         protoArgs = list(name = NULL
-                                                          , description = NULL
-                                                          , expectedValueType = NA
-                                                          , expectedValues = NA
-                                                          , defaultValue = "<none>"))
-                   , check = defaultArgumentCheck
-                   , funEnvir = FALSE)
+                                        protoArgs = list(name = NULL
+                                                         , description = NULL
+                                                         , expectedValueType = "any"
+                                                         , expectedValues = NA
+                                                         , defaultValue = "<none>"))
+                  , check = defaultArgumentCheck
+                  , funEnvir = FALSE)
 
 
 # Backup Argument definition ----------------------------------------------
--- rapid/test_objects.R
+++ rapid/test_objects.R
@@ -1,23 +1,64 @@
-library(testthat)
 rm(list = ls())
+
+library(testthat)
 source("rapid/objects.R")
 
 context("Argument trait")
+rapid.action <- "/test"
 
 test_that("the backup Argument trait constructor is well defined", {
+  
   expect_error(ArgumentBckp$new(), "PROTO: You created an instance of the Trait 'Argument' without specifying the needed parameter 'name'.")
   expect_error(ArgumentBckp$new(name = ""), "PROTO: You created an instance of the Trait 'Argument' without specifying the needed parameter 'description'.")
+  
 })
 
+
 test_that("the Argument trait constructor is well defined", {
+  
   expect_error(Argument$new(), "PROTO: You created an instance of the Trait 'Argument' without specifying the needed parameter 'name'.")
   expect_error(Argument$new(name = ""), "PROTO: You created an instance of the Trait 'Argument' without specifying the needed parameter 'description'.")
   arg1 <- Argument$new(name = "arg1", description = "An argument")
   expect_equal(arg1$expectedValues, NA)
   expect_equal(arg1$defaultValue, "<none>")
-  })
+  
+})
 
-# test_that("the Argument ")
+
+test_that("the default check function behaves correctly when there is no expectedValueType specified", {
+  
+  miscArg <- Argument$new(name = "miscArg", description = "")
+  expect_equal(miscArg$check("5"), "5")
+  expect_equal(miscArg$check("A string"), "A string")
+  expect_equal(miscArg$check(""), "")
+  
+  withinArg <- Argument$new(name = "miscArg", description = "", expectedValues = c("A string", "Another string"))
+  expect_equal(withinArg$check("A string"), "A string")
+  expect_equal(withinArg$check("Another string"), "Another string")
+  expect_error(withinArg$check("5"), "ARGS: Wrong value for")
+  expect_error(withinArg$check(""), "ARGS: Wrong value for")
+  
+})
+
+
+test_that("the default check function behaves correctly when the expectedValueType is integer", {
+  
+  miscArg <- Argument$new(name = "miscArg", description = "", expectedValueType = "integer")
+  expect_equal(miscArg$check("5"), 5)
+  expect_error(miscArg$check("A string"), "ARGS: Wrong value type for")
+  expect_equal(miscArg$check(""), NA)
+  
+  withinArg <- Argument$new(name = "withinArg", description = "", expectedValues = c(5, 6), expectedValueType = "integer")
+  expect_equal(withinArg$check("5"), 5)
+  expect_error(withinArg$check("8"), "ARGS: Wrong value")
+  expect_equal(withinArg$check(""), NA)
+  
+  rangeArg <- Argument$new(name = "rangeArg", description = "", expectedValues = list(min = 4, max = 6), expectedValueType = "integer")
+  expect_equal(rangeArg$check("5"), 5)
+  expect_error(rangeArg$check("8"), "ARGS: Value is not in range")
+  expect_equal(rangeArg$check(""), NA)
+  
+})
 
 # 
 # rapid.action <- "/helloWorld"
